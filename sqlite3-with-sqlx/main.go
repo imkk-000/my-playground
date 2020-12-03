1bdccfa57cd67178563f2f9c46563029df84ef84
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-12-03 20:55:51 +0700

    Add sqlx playground

---
+++ sqlite3-with-sqlx/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("Failed to %s: %s", msg, err.Error())
+	}
+}
+
+type Entity struct {
+	ID          sql.NullInt32 `db:"id"`
+	CreatedTime sql.NullTime  `db:"created_time"`
+}
+
+func main() {
+	conn, err := sqlx.Open("sqlite3", "file:sqlite.db")
+	failOnError(err, "create db")
+
+	err = conn.Ping()
+	failOnError(err, "ping db")
+
+	stmt, err := conn.Preparex("SELECT * FROM timer")
+	failOnError(err, "prepare statement")
+	defer stmt.Close()
+
+	rows, err := stmt.Queryx()
+	failOnError(err, "query row")
+	defer rows.Close()
+
+	for rows.Next() {
+		var entity Entity
+		err := rows.StructScan(&entity)
+		failOnError(err, "scan row")
+
+		log.Printf("struct: %+v", entity)
+		log.Println(entity.ID.Value())
+		log.Println(entity.CreatedTime.Value())
+		log.Printf("id=%d, created_time=%v", entity.ID.Int32, entity.CreatedTime.Time)
+	}
+}
