80abaed9bd3b796214af49fa328822ddd0f2b260
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-12-08 20:59:35 +0700

    Add elapsed time

--- go-channel-with-for-range/main.go
+++ go-channel-with-for-range/main.go
@@ -3,31 +3,35 @@ package main
 import (
 	"log"
 	"sync"
+	"time"
 )
 
 func main() {
+	startTime := time.Now()
+
 	dataChan := make(chan struct{})
 
 	type Counter struct {
 		sync.Mutex
 		value int
 	}
-
-	var counter int
+	var counter = new(Counter)
 	var waitGroup = new(sync.WaitGroup)
 
 	// create workers
 	for i := 1; i <= 100; i++ {
 		// consumer
-		go func(workerNo int, wg *sync.WaitGroup, c *int, ch chan struct{}) {
+		go func(workerNo int, wg *sync.WaitGroup, c *Counter, ch chan struct{}) {
 			defer wg.Done()
 			wg.Add(1)
 
 			for v := range dataChan {
 				log.Printf("value #%d: %+v\n", workerNo, v)
-				*c++
+				c.Lock()
+				c.value++
+				c.Unlock()
 			}
-		}(i, waitGroup, &counter, dataChan)
+		}(i, waitGroup, counter, dataChan)
 	}
 
 	// producer
@@ -37,6 +41,8 @@ func main() {
 
 	close(dataChan)
 	waitGroup.Wait()
+
+	log.Printf("elapsed time: %s\n", time.Since(startTime))
 	log.Printf("total consumer: %d\n", counter)
 	log.Println("terminating...")
 }
