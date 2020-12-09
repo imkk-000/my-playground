74fd7a5592e345422b8541120833acd214dcfd09
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-12-09 19:27:18 +0700

    Add playground test container lib

---
+++ go-test-container/go.mod
@@ -0,0 +1,5 @@
+module playground
+
+go 1.15
+
+require github.com/testcontainers/testcontainers-go v0.9.0
---
+++ go-test-container/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func main() {
+	mu := sync.Mutex{}
+	mu.Lock()
+
+	t := &testing.T{}
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "redis:latest",
+		ExposedPorts: []string{"6379/tcp"},
+		WaitingFor:   wait.ForLog("Ready to accept connections"),
+	}
+	redisC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+
+		t.Error(err)
+	}
+	defer redisC.Terminate(ctx)
+}
