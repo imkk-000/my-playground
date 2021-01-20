3f4894505612705f9f882bec2d031b4ed8db7c8d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2021-01-20 16:23:56 +0700

    Update playground go1.6 embed

--- go-embedding-file/main.go
+++ go-embedding-file/main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"embed"
+	"log"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 	//go:embed hello.txt
-	var s string
-	print(s)
-	println()
+	//go:embed html/* image/*
+	var content embed.FS
+	log.Println(content.ReadFile("hello.txt"))
+	dirs, err := content.ReadDir("html")
+	failOnError(err, "fail to read dir")
+	for _, dir := range dirs {
+		info, err := dir.Info()
+		failOnError(err, "fail to get file info")
+		log.Printf("print info: %+v", info)
+	}
+	dirs, err = content.ReadDir("image")
+	failOnError(err, "fail to read dir")
+	for _, dir := range dirs {
+		info, err := dir.Info()
+		failOnError(err, "fail to get file info")
+		log.Printf("print info: %+v", info)
+	}
 
-	//go:embed hello.txt
-	var b []byte
-	print(string(b))
-	println()
-
-	//go:embed hello.txt
-	var f embed.FS
-	data, _ := f.ReadFile("hello.txt")
-	print(string(data))
-	println()
+	//go:embed mock_main.go
+	var contentFile string
+	log.Println(contentFile)
 }
